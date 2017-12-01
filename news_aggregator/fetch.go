1e2bc48c38c40e76396d63537077c336304a7e05
Bogdan <bogdan.mihai.timofte@gmail.com>
2017-12-01 18:59:37 +0200

    fetch xml and parse it into structs

--- news_aggregator/fetch.go
+++ news_aggregator/fetch.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
+// Location represents a description for an element from Sitemap
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+// SitemapIndex is the abdtract parsed form of xml sitemap
+type SitemapIndex struct {
+	Locations []Location `xml:"sitemap"` // A slice
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
+
 func main() {
 	response, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
 	bytes, _ := ioutil.ReadAll(response.Body)
-	stringBody := string(bytes)
 	response.Body.Close()
-	fmt.Println(stringBody)
+
+	var s SitemapIndex
+	xml.Unmarshal(bytes, &s)
+
+	fmt.Println(s.Locations)
 }
